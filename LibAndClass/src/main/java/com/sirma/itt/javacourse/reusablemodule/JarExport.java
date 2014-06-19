8620262e229093b5a9f17e8a912714498c022667
radko26 <radipirate@gmail.com>
2014-06-19 23:03:27 +0300

    added

---
+++ LibAndClass/src/main/java/com/sirma/itt/javacourse/reusablemodule/JarExport.java
@@ -0,0 +1,29 @@
+package main.java.com.sirma.itt.javacourse.reusablemodule;
+
+/**
+ * Class which run the introToJava's main methods from a jar file.
+ * 
+ * @author radoslav
+ */
+
+public class JarExport {
+
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		try{
+			main.java.com.sirma.itt.javacourse.intro.hangman.main.Main.main(args);
+			main.java.com.sirma.itt.javacourse.intro.arrayutils.main.Main.main(args);
+			main.java.com.sirma.itt.javacourse.intro.bigint.Main.main(args);
+			main.java.com.sirma.itt.javacourse.intro.gcd.Main.main(args);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
